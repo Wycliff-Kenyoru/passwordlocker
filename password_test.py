0d78794decaf9ae9b183bdd504e00baca2bc4712
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-10-15 09:22:06 +0300

    class test password and Setup

--- password_test.py
+++ password_test.py
@@ -2,3 +2,13 @@
 import pyperclip
 import unittest #iporting unitest module
 from password import Password #importing password module
+
+class TestPassword(unittest.TestCase):
+    # thsi class defines test cases for password class behaviourspython3.6 contact_test.pypython3.6 contact_test.py
+
+
+    # also helps ccreating test cases
+
+    def setUP(self):
+        # method that runs before each test
+        self.new_password("wycliff", "kerongo","wycliffkerongogmail.com","110p05124h")
