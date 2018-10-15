d483f91b7cc36a3c62354ada10d1fd78024489a6
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-10-15 09:36:39 +0300

    final

--- password_test.py
+++ password_test.py
@@ -26,4 +26,51 @@ class TestPassword(unittest.TestCase):
         self.new_password.save_password()  # saving new password
         self.assertEqual(len(Password.password_list), 1)
 
+    def test_save_multiple_password(self):
+        # checks if we can save multiple passwords to our list
+        self.new_password.save_password()
+        test_password = Password("Test", "user", "wycliffkerongo@gmail.com", "110p05124h")
+        test_password.save_password()
+        self.assertEqual(len(Password.password_list), 1)
 
+    def test_delete_password(self):
+        # check if we can remove password from our list
+        self.password.save_password()
+        test_password = Password("Test", "user", "wycliffkerongo@gmail.com", "110p05124h")
+        test_password.save_password()
+        self.new_password.delete_password()  # deleting password
+        self.assertEqual(len(Password.password_list), 1)
+
+    def test_find_by_email(self):
+        # checking if the password is in the list and the display the info
+        self.new_password.save_password()
+        test_password = Password("Test", "user", "duncanarani@gmail.com", "31740141")
+        test_password.save_password()
+        found_password = Password.find_by_email("31740141")
+
+        self.assertEqual(found_password.email, password.email)
+
+    def test_password_exists(self):
+        # checking if we return the booleon if the passoword is not found
+        self.new_password.Password()
+        test_password = Password("Test", "user", "duncanarani@gmail.com", "31740141")
+        test_password.save_password()
+
+        password_exists = Password.password_exists("31740141")
+        self.assertTrue(password_exists)
+
+    def test_display_all_passwords(self):
+        # this returns alist of all password saved
+        self.assertEqual(Password.display_password(), Password.password_list)
+
+    def test.
+
+        copy_email(self)
+    # confirm that we are copying the email from the found password
+    self.new_password.save_password()
+    Password.copy_email("password")
+
+    assertEqual(self.new_password.email, pyperclip.paste())
+
+    if __name__ == '__main__':
+        unittest.main()
